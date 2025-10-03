67dbd8c3c4083a4dfc334f4b056a7f67ec3fe665
GBS-Alex <43650742+GBS-Alex@users.noreply.github.com>
2025-10-03 13:14:21 -0700

    fix scale of front, box shadows, and improve 3d appearance

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import vInside from "./assets/vInside.png";
 import vInsideBlank from "./assets/vInsideBlank.png";
 import vCover from "./assets/vCover.png";
 import vBack from "./assets/vBack.png";
-// import hInside from "./assets/hInside.png";
-// import hCover from "./assets/hCover.png";
-// import hBack from "./assets/hBack.png";
+import hInside from "./assets/hInside.png";
+import hCover from "./assets/hCover.png";
+import hBack from "./assets/hBack.png";
 
 function App() {
 	const [perspective, setPerspective] = useState<number>(0);
@@ -22,24 +22,25 @@ function App() {
 	const isTransitioningFrom2to0 = previousPerspective === 2 && perspective === 0;
 
 	return (
-		<div className="container">
+		<div>
 			<p>Perspective: {perspective}</p>
 			<button onClick={nextPerspective}>Next</button>
 			<br></br>
-
-			<div
-				className={`card perspective${perspective} ${
-					isTransitioningFrom2to0 ? "from-perspective2" : ""
-				}`}>
-				<img src={vCover} className="front" />
-				<img src={vInsideBlank} className="back" />
-			</div>
-			<div
-				className={`backCard card perspective${perspective} ${
-					isTransitioningFrom2to0 ? "from-perspective2" : ""
-				}`}>
-				<img src={vInside} className="front" />
-				<img src={vBack} className="back" />
+			<div className="container">
+				<div
+					className={`frontCard card perspective${perspective} ${
+						isTransitioningFrom2to0 ? "from-perspective2" : ""
+					}`}>
+					<img src={vCover} className="front" />
+					<img src={vInsideBlank} className="back" />
+				</div>
+				<div
+					className={`backCard card perspective${perspective} ${
+						isTransitioningFrom2to0 ? "from-perspective2" : ""
+					}`}>
+					<img src={vInside} className="front" />
+					<img src={vBack} className="back" />
+				</div>
 			</div>
 		</div>
 	);
